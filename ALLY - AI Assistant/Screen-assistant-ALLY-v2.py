f3dd379c18b3abc9b9f62a5a369e5c00cf1a02c2
Alexander Hansen <164519860+Holmbrg@users.noreply.github.com>
2024-10-06 21:14:16 +0200

    Update Screen-assistant-ALLY-v2.py

--- ALLY - AI Assistant/Screen-assistant-ALLY-v2.py
+++ ALLY - AI Assistant/Screen-assistant-ALLY-v2.py
@@ -34,6 +34,7 @@ os.environ['GRPC_VERBOSITY'] = 'ERROR'
 warnings.filterwarnings("ignore")
 logging.getLogger().setLevel(logging.ERROR)
 pytesseract.pytesseract.tesseract_cmd = r'C:\Programmer\Tesseract-OCR\tesseract.exe'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'C:\\ALLY - AI Assistant\\fabled-gist-429918-k7-3f2dfb1ef283.json'  # Add Google API key here, see README.txt
 
 class ScreenshotHandler(FileSystemEventHandler):
     """
