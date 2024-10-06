3969d859e43ce777388b3f60ebbe3c1214f6cc90
Alexander Hansen <164519860+Holmbrg@users.noreply.github.com>
2024-10-06 21:06:27 +0200

    Update Voice-assistant-ALLY-v3.py

--- ALLY - AI Assistant/Voice-assistant-ALLY-v3.py
+++ ALLY - AI Assistant/Voice-assistant-ALLY-v3.py
@@ -46,7 +46,6 @@ os.environ['GRPC_VERBOSITY'] = 'ERROR'
 warnings.filterwarnings("ignore")
 logging.getLogger().setLevel(logging.ERROR)
 pytesseract.pytesseract.tesseract_cmd = r'C:\Programmer\Tesseract-OCR\tesseract.exe'
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'C:\\ALLY - AI Assistant\\fabled-gist-429918-k7-3f2dfb1ef283.json'  # Add Google API key here, see README.txt
 
 if not os.path.exists(conversation_log):
     with open(conversation_log, 'w') as log:
